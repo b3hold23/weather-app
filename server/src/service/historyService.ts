0e9d04dca644a29938aa50443a82569c63085d3f
unknown <rosa.angel.daniel@gmail.com>
2024-10-10 17:13:48 -0600

    began working on history service function

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 class City {
   name: string;
   id: string;
-  
+
   constructor(name: string, id: string) {
     this.name = name;
     this.id = id;
@@ -15,9 +15,16 @@ class City {
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read() {
+    return await fs.promises.readFile( 'db/searchHistory.json', {
+      flag: 'a+',
+      encoding: 'utf-8',
+    });
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+    return async fs.promises.writeFile('db/searchHistory.json', JSON.stringify(cities, null, '\t'));
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   // async getCities() {}
   // TODO Define an addCity method that adds a city to the searchHistory.json file
